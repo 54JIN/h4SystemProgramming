802c183bace15535fe25031f11d55e6cd461c4b8
54JIN <72236983+54JIN@users.noreply.github.com>
2022-12-12 03:43:07 -0500

    Version 3
    Updated the names of malloc and realloc to mymalloc and myrealloc

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
-#include <stdlib.h>
+//Compile the main.c with mymalloc.c file with this on the command: gcc -Wall -Wextra main.c mymalloc.c -o main && ./main
 #include <stdio.h>
-#include <stddef.h>
-#include "mymalloc2.h"
+#include "mymalloc.h"
 
 int main(void) {
   // Allocate a block of memory with the specified size
-  void *ptr = malloc(1024);
+  void *ptr = mymalloc(1024);
 
   // Print the address of the allocated memory
   printf("ptr = %p\n", ptr);
 
   // Resize the allocated block of memory to the specified size
-  ptr = realloc(ptr, 2048);
+  ptr = myrealloc(ptr, 2048);
 
   // Print the address of the resized memory
   printf("ptr = %p\n", ptr);
